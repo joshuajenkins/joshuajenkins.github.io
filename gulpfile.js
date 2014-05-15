4e39926641bf41b1c0c9fb951cf8d1243bc90e18
Joshua Jenkins <joshuajenkins@me.com>
2014-05-14 22:44:17 -0700

    10k draft

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,42 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var minifyCSS = require('gulp-minify-css');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    minifyCSS = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat')
 
 gulp.task('sass', function() {
 	return gulp.src('src/scss/*.scss')
 	.pipe(sass())
-	.pipe(minifyCSS(
-		{'keepSpecialComments': 0}
-	))
+	.pipe(minifyCSS({'keepSpecialComments': 0}))
 	.pipe(gulp.dest('css'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('src/scss/*.scss', ['sass']);
+gulp.task('10k', function() {
+  gulp.src(['components/moment/moment.js', 'components/pikaday/pikaday.js'])
+  .pipe(concat('lib.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('static/10k/'));
+
+  gulp.src(['components/pikaday/css/pikaday.css'])
+  .pipe(minifyCSS({'keepSpecialComments': 0}))
+  .pipe(gulp.dest('static/10k/'));
+
+  gulp.src(['src/scss/10k/10k.scss'])
+  .pipe(sass())
+  .pipe(minifyCSS({'keepSpecialComments': 0}))
+  .pipe(gulp.dest('static/10k/'));
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('watch', function() {
+  // Sitewide
+	gulp.watch('src/scss/*.scss', ['sass']);
+
+  // 10k
+  gulp.watch(['components/moment/moment.js',
+    'components/pikaday/pikaday.js',
+    'components/pikaday/css/pikaday.css',
+    'src/scss/10k/*.scss'],
+  ['10k']);
+});
+
+gulp.task('default', ['sass', 'watch', '10k']);
---
+++ static/10k/10k.js
@@ -0,0 +1,29 @@
+var processResults = function(birthdate) {
+  var a = birthdate.add('days', 10000);
+  var b = moment();
+  var delta = parseInt(a.diff(b, 'days')) + 1;
+  var formattedDate = b.add('days', delta).format('MMMM Do, YYYY');
+
+  if (delta > 0) {
+    $('#10k-module .results').html('<p>Your 10,000th day is on ' + formattedDate + '.<br>Only ' + delta + ' days to go.</p>');
+  } else if (delta < 0) {
+    $('#10k-module .results').html('<p>Your 10,000th day was on ' + formattedDate + '.</p>');
+  } else {
+    $('#10k-module .results').html('<p>Your 10,000th day is today!<br>You should probably reflect.</p>');
+  }
+};
+
+$(document).ready(function() {
+  var picker = new Pikaday({
+    field: $('#10k-datepicker')[0],
+    minDate: moment("1900", "YYYY").toDate(),
+    maxDate: moment().toDate(),
+    yearRange: 100,
+    format: 'MMMM Do, YYYY',
+    position: 'bottom',
+    onSelect: function() {
+      var result = this.getMoment();
+      processResults(result);
+    }
+  });
+});
