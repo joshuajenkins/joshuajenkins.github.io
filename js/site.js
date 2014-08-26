fa91cdb8665720aae41726e625cbe6cabf8f07c7
Joshua Jenkins <josh@joshuajenkins.com>
2014-08-25 23:43:48 -0700

    Let's get international

--- js/site.js
+++ js/site.js
@@ -1,10 +1,14 @@
+var monies = ['$', '¥', '£', '€'];
+var real = 'oshua Jenkins';
+
 function beTrashy() {
-  var real = 'oshua Jenkins';
   var el = document.getElementById('name');
   var text = el.innerText;
-  var rando = Math.floor(Math.random() * (text.length));
-  if (text.charAt(rando) != '$') {
-    var newText = text.substr(0, rando) + '$' + text.substr(rando+1);
+  var rando = Math.floor(Math.random() * text.length);
+  var currRando = monies[Math.floor(Math.random() * monies.length)];
+
+  if (monies.indexOf(text.charAt(rando)) == -1) {
+    var newText = text.substr(0, rando) + currRando + text.substr(rando+1);
   } else {
     var newText = text.substr(0, rando) + real.charAt(rando) + text.substr(rando+1);
   }
@@ -13,5 +17,5 @@ function beTrashy() {
 
 // Works in not IE
 document.addEventListener("DOMContentLoaded", function() {
-  window.setInterval(beTrashy, 400);
+  window.setInterval(beTrashy, 1000);
 });
