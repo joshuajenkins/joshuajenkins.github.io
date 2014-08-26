3a77725d7d1806d791d3b391b133083c2fcf5106
Joshua Jenkins <josh@joshuajenkins.com>
2014-08-26 08:08:18 -0700

    Remove old codez

--- js/site.js
+++ js/site.js
@@ -1,17 +1,7 @@
-var monies = ['$', '¥', '£', '€'];
-var real = 'oshua Jenkins';
-
 function beTrashy() {
   var el = document.getElementById('name');
   var text = el.innerText;
   var rando = Math.floor(Math.random() * text.length);
-  var currRando = monies[Math.floor(Math.random() * monies.length)];
-
-  // if (monies.indexOf(text.charAt(rando)) == -1) {
-  //   var newText = text.substr(0, rando) + currRando + text.substr(rando+1);
-  // } else {
-  //   var newText = text.substr(0, rando) + real.charAt(rando) + text.substr(rando+1);
-  // }
 
   if (rando == 1 || rando == 12) {
     var newChar = (text.charAt(rando) == '$') ? 's' : '$';
