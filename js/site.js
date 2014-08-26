a5b2adbec1ec8e3da614d167ddf9571c2193656e
Joshua Jenkins <josh@joshuajenkins.com>
2014-08-26 08:07:11 -0700

    Less gratiutous but still very gratiutous

--- js/site.js
+++ js/site.js
@@ -7,15 +7,26 @@ function beTrashy() {
   var rando = Math.floor(Math.random() * text.length);
   var currRando = monies[Math.floor(Math.random() * monies.length)];
 
-  if (monies.indexOf(text.charAt(rando)) == -1) {
-    var newText = text.substr(0, rando) + currRando + text.substr(rando+1);
-  } else {
-    var newText = text.substr(0, rando) + real.charAt(rando) + text.substr(rando+1);
+  // if (monies.indexOf(text.charAt(rando)) == -1) {
+  //   var newText = text.substr(0, rando) + currRando + text.substr(rando+1);
+  // } else {
+  //   var newText = text.substr(0, rando) + real.charAt(rando) + text.substr(rando+1);
+  // }
+
+  if (rando == 1 || rando == 12) {
+    var newChar = (text.charAt(rando) == '$') ? 's' : '$';
+  } else if (rando == 6) {
+    var newChar = (text.charAt(rando) == '¥' ? 'J' : '¥');
+  } else if (rando == 7) {
+    var newChar = (text.charAt(rando) == '€' ? 'e' : '€');
   }
-  el['innerText'] = newText;
+
+  if (newChar) {
+    el['innerText'] = text.substr(0, rando) + newChar + text.substr(rando+1);
+  }
 }
 
 // Works in not IE
 document.addEventListener("DOMContentLoaded", function() {
-  window.setInterval(beTrashy, 1000);
+  window.setInterval(beTrashy, 400);
 });
