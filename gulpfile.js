5068f1b22a9096101a402bd3d58f24c9014213f6
Joshua Jenkins <josh@joshuajenkins.com>
2014-11-28 14:24:49 -0800

    Remove 10k post because it's dumb

--- gulpfile.js
+++ gulpfile.js
@@ -11,27 +11,8 @@ gulp.task('sass', function() {
 	.pipe(gulp.dest('css'));
 });
 
-gulp.task('10k', function() {
-  gulp.src(['components/moment/moment.js', 'components/pikaday/pikaday.js'])
-  .pipe(concat('lib.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('static/10k/'));
-
-  gulp.src(['src/scss/10k/10k.scss'])
-  .pipe(sass())
-  .pipe(minifyCSS({'keepSpecialComments': 0}))
-  .pipe(gulp.dest('static/10k/'));
-});
-
 gulp.task('watch', function() {
-  // Sitewide
 	gulp.watch('src/scss/*.scss', ['sass']);
-
-  // 10k
-  gulp.watch(['components/moment/moment.js',
-    'components/pikaday/pikaday.js',
-    'src/scss/10k/*.scss'],
-  ['10k']);
 });
 
-gulp.task('default', ['sass', 'watch', '10k']);
+gulp.task('default', ['sass', 'watch']);
